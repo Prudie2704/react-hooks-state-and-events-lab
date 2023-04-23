6dbd480c622874e901006b2b7814730adc7c0908
prudence <prudence.jerono@student.moringaschool.com>
2023-04-23 22:47:50 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -4,3 +4,30 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleToggleDarkMode() {
+    setIsDarkMode(!isDarkMode);
+  }
+
+  return (
+    <div className={isDarkMode ? "dark" : "light"}>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleToggleDarkMode}>
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
